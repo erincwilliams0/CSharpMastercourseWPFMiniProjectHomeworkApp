5b244f7b7422df9295f67afdf060579dcc36e44c
erin williams <erin.cwilliams0@gmail.com>
2025-06-30 23:51:48 -0500

    Add project files.

---
+++ DemoLibrary/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+
+        public List<AddressModel> AddressList { get; set; }
+    }
+}
